6692f97f6a04480dece3d9899a5f212b9b979912
Emma Hack <ehack78@gmail.com>
2021-01-12 20:39:28 +0000

    ISecurity implemented in Tobacco Stall and all tests passed

--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,21 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        visitor1 = new Visitor(20, 186, 25.43);
+        visitor2 = new Visitor(15, 201, 14.63);
     }
 
     @Test
@@ -27,5 +32,15 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void customerCanPurchase__True(){
+        assertEquals(true, tobaccoStall.isAllowedTo(visitor1));
+    }
+
+    @Test
+    public void customerCanPurchase__False(){
+        assertEquals(false, tobaccoStall.isAllowedTo(visitor2));
     }
 }
