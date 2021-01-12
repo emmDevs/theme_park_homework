e489e5c0839cc682a8f0c99225abcbbcdbfec3ce
Emma Hack <ehack78@gmail.com>
2021-01-12 22:37:56 +0000

    visitedAttractions arraylist added to visitor and tests passw ed

--- src/test/java/people/VisitorTest.java
+++ src/test/java/people/VisitorTest.java
@@ -1,18 +1,32 @@
 package people;
 
+import attractions.Dodgems;
+import attractions.Playground;
+import attractions.RollerCoaster;
 import org.junit.Before;
 import org.junit.Test;
+import stalls.CandyflossStall;
+import stalls.IceCreamStall;
+import stalls.ParkingSpot;
+import stalls.TobaccoStall;
 
 import static org.junit.Assert.assertEquals;
 
 public class VisitorTest {
 
     Visitor visitor;
+    Dodgems dodgems;
+    Playground playground;
+    RollerCoaster rollerCoaster;
 
     @Before
     public void before(){
         visitor = new Visitor(14, 1.2, 40.0);
+        dodgems = new Dodgems("Bumper Cars", 5);
+        playground = new Playground("Fun Zone", 7);
+        rollerCoaster = new RollerCoaster("Blue Ridge", 10);
     }
+
 
     @Test
     public void hasAge() {
@@ -27,5 +41,18 @@ public class VisitorTest {
     @Test
     public void hasMoney() {
         assertEquals(40.0, visitor.getMoney(), 0.1);
+    }
+
+    @Test
+    public void visitedAttractionsStartsAt0(){
+        assertEquals(0, visitor.visitedAttractionsCount());
+    }
+
+    @Test
+    public void canAddAttractionToVisitedAttractions(){
+        visitor.addAttraction(dodgems);
+        visitor.addAttraction(playground);
+        visitor.addAttraction(rollerCoaster);
+        assertEquals(3, visitor.visitedAttractionsCount());
     }
 }
