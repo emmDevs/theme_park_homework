cc299c2a972c4db534a6b68fb59f177179bba895
Emma Hack <ehack78@gmail.com>
2021-01-12 21:49:34 +0000

    theme park returns arraylist and can add attractions and stalls tests passed

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,24 @@
+import behaviours.IReviewed;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    private ArrayList<IReviewed> attractionsAndStalls;
+
+    public ThemePark(){
+        this.attractionsAndStalls = new ArrayList<IReviewed>();
+    }
+
+    public ArrayList<IReviewed> getAllReviewed(){
+        return this.attractionsAndStalls;
+    }
+
+    public int attractionAndStallCount(){
+        return this.attractionsAndStalls.size();
+    }
+
+    public void addAttractionOrStall(IReviewed attractionOrStall){
+        this.attractionsAndStalls.add(attractionOrStall);
+    }
 }
