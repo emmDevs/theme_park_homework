d4b4030c08613661f9b54fedc1212743a2a43dcd
Emma Hack <ehack78@gmail.com>
2021-01-12 20:24:50 +0000

    ITicketed implemented for Dodgems and all tests passed

--- src/main/java/attractions/RollerCoaster.java
+++ src/main/java/attractions/RollerCoaster.java
@@ -1,8 +1,17 @@
 package attractions;
 
+import people.Visitor;
+
 public class RollerCoaster  extends Attraction {
 
     public RollerCoaster(String name, int rating) {
         super(name, rating);
     }
+
+//    @Override
+//    public double priceFor(Visitor visitor) {
+//        if(visitor != null && visitor.getHeight() > 200) {
+//        }
+//        return (defaultPrice() * 2);
+//    }
 }
--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,22 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        visitor1 = new Visitor(10, 146, 5.43);
+        visitor2 = new Visitor(15, 201, 14.63);
+
     }
 
 
@@ -29,4 +35,26 @@ public class DodgemTest {
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
     }
+
+    @Test
+    public void hasDefaultPrice(){
+        assertEquals(4.50, dodgems.defaultPrice(), 0.01);
+    }
+
+    @Test
+    public void canGetPriceForVisitor__under12YearsOld(){
+        assertEquals(2.25, dodgems.priceFor(visitor1), 0.01);
+    }
+
+    @Test
+    public void canGetPriceForVisitor__over12YearsOld(){
+        assertEquals(4.50, dodgems.priceFor(visitor2), 0.01);
+    }
+
+    @Test
+    public void canGetPriceForVisitor__null(){
+        assertEquals(0.00, dodgems.priceFor(null), 0.01);
+    }
+
+
 }
